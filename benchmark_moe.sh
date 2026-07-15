69f7b10d333bc5ef61d6731f790c0aaf06e3fd6d
danh-real <htduyanh210604@gmail.com>
2026-07-15 02:52:09 +0000

    training using adamw

--- benchmark_moe.sh
+++ benchmark_moe.sh
@@ -2,4 +2,3 @@ CUDA_VISIBLE_DEVICES=1 python benchmark_moe.py \
     --data-root /data/datasets/AnyEdit \
     --output-dir /data/repos/models/0-output/AnyEdit-Test \
     --edit-file /data/datasets/AnyEdit/edit.json \
-    --lora-path runs/train_moe_lora_stage1overfit/20260711-071003/ckpt/50000
