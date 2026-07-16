941e94ebeea5f5495ce60772ed0f7ce099b89833
danh-real <htduyanh210604@gmail.com>
2026-07-16 02:47:35 +0000

    training stage2

--- benchmark_moe.sh
+++ benchmark_moe.sh
@@ -1,4 +1,6 @@
-CUDA_VISIBLE_DEVICES=1 python benchmark_moe.py \
+CUDA_VISIBLE_DEVICES=0 python benchmark_moe.py \
     --data-root /data/datasets/AnyEdit \
     --output-dir /data/repos/models/0-output/AnyEdit-Test \
     --edit-file /data/datasets/AnyEdit/edit.json \
+    --lora-path runs/train_moe_lora_stage1overfit_softmax/20260714-010507/ckpt/30000 \
+    --saved-suffix-model "stage1-softmax"
